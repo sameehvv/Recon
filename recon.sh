8f7b19a36d833b5670c448a1ea9b1866a4cbef4c
Sameeh <77770369+sameehvv@users.noreply.github.com>
2021-07-27 16:50:39 +0530

    Create recon.sh

---
+++ recon.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+#correct syntax
+if [ "$1" == "" ]
+then
+    echo "[+]Syntax:- $0 example.com"
+else
+
+    url=$1
+
+    #making directory
+    mkdir $url
+
+    #gathering the subdomains
+    echo "[+]Gathering subdomains using assetfinder"
+    assetfinder -subs-only $url >> subs.txt
+    echo "Done"
+
+    echo "[+]Gathering subdomains using subfinder"
+    subfinder -d $url -silent >> subs.txt
+    echo "Done"
+
+    echo "[+]Gathering subdomains using amass"
+    amass enum -norecursive -noalts -d $url >> subs.txt
+    echo "Done"
+
+    echo -e "\n[+]Found $(cat subs.txt | wc -l) subdomains"
+     
+    #finding the alive domains
+    echo -e "\n[+]Finding live subdomains"
+    cat subs.txt | httprobe >> alive.txt
+     
+    #deduplicates and sorting
+    while read line; do
+        echo ${line#*://} >> newlist.txt
+    done < alive.txt
+
+    cat newlist.txt | sort -u >> $url/subdomains.txt
+    echo "$(cat $url/subdomains.txt | wc -l) subdomains found"
+
+    #subdomains takeoever
+    echo -e "\n[+]Testing subdomains takeover"
+    subjack -w $url/subdomains.txt -t 100 -timeout 30 -ssl -c ~/go/src/github.com/haccer/subjack/fingerprints.json -v 3 >> $url/takeover.txt
+    echo "Done"
+
+    #waybackurls
+    echo -e "\n[+]Scraping wayback data"
+    cat $url/subdomains.txt | waybackurls | sort -u >> $url/waybackdata.txt   
+    echo "Done"
+
+    #aquatone
+    echo -e "\n[+]Running aquatone on subdomains"
+    cat $url/subdomains.txt | aquatone -out $url/aquatone
+    echo "Done"
+
+    echo "All files saved to '$url' folder"
+
+    #removing unwanted files
+    rm subs.txt alive.txt newlist.txt 
+fi
