6419b575c6ba7f6c2aa2cdf1fbbfe3c825032d54
Sameeh <77770369+sameehvv@users.noreply.github.com>
2021-10-25 13:30:24 +0530

    Update recon.sh

--- recon.sh
+++ recon.sh
@@ -3,13 +3,15 @@
 #correct syntax
 if [ "$1" == "" ]
 then
-    echo "[+]Syntax:- $0 example.com"
+    echo "$0 example.com"
 else
 
     url=$1
-
-    #making directory
     mkdir $url
+
+    #Github_Dorks
+    ~/toolkit/gdorklinks/Gdorklinks.sh $url > $url/github_dorks.txt
+    echo -e "\n[+]Github dorks links saved to "$url/github_dorks.txt
 
     #gathering the subdomains
     echo "[+]Gathering subdomains using assetfinder"
@@ -36,7 +38,16 @@ else
     done < alive.txt
 
     cat newlist.txt | sort -u >> $url/subdomains.txt
-    echo "$(cat $url/subdomains.txt | wc -l) subdomains found"
+    cat $url/subdomains.txt | ts https:// | tr -d ' ' >> $url/http_subdomains.txt
+    echo "$(cat $url/subdomains.txt | wc -l) Subdomains Found"
+
+    #collecting all IP from subdomains
+    echo -e "\n[+]Saving IPs to "$url/ip.txt
+    cat $url/subdomains.txt | dnsprobe -silent | sort -u > $url/ip.txt
+
+    #Running whatweb
+    echo -e "\n[+]Running Whatweb"
+    whatweb -i $url/subdomains.txt | tee -a $url/whatweb_op.txt
 
     #subdomains takeoever
     echo -e "\n[+]Testing subdomains takeover"
@@ -47,16 +58,20 @@ else
     echo -e "\n[+]Fetching URLs"
     cat $url/subdomains.txt | waybackurls >> waybackdata.txt
     cat $url/subdomains.txt | gau >> gaudata.txt
-    cat waybackdata.txt gaudata.txt | sort -u >> $url/urls.txt   
+    cat waybackdata.txt gaudata.txt | sort -u >> $url/urls.txt 
     echo "Done"
+
+    #running grapX
+    echo -e "\n[+]Running grapX"
+    python2 ~/toolkit/grapX/grapX $url/urls.txt $url/grapX_op.txt
 
     #aquatone
     echo -e "\n[+]Running aquatone on subdomains"
-    cat $url/subdomains.txt | httprobe | aquatone -out $url/aquatone
+    cat $url/subdomains.txt| httprobe | aquatone -out $url/aquatone
     echo "Done"
 
-    echo "All files saved to '$url' folder"
+    echo -e "\n[+]All files saved to '$url' folder"
 
     #removing unwanted files
-    rm subs.txt alive.txt newlist.txt 
+    rm subs.txt alive.txt newlist.txt waybackdata.txt gaudata.txt
 fi
